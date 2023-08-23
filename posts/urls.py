2f9ecccfa6f6340f163451ca9d0e6c9219a71b36
parkjiwoo <dndjf1109@gmail.com>
2023-08-23 14:50:35 +0900

    uodate기능

--- posts/urls.py
+++ posts/urls.py
@@ -10,7 +10,11 @@ urlpatterns=[
     #create
     path('new/', views.new, name='new') ,
     path('create/', views.create, name='create') ,
-
+    
+    #delete
     path('<int:id>/delete/', views.delete, name='delete') ,
+    #update
+    path('<int:id>/edit/',views.edit, name='edit'),
+    path('<int:id>/update/',views.update, name='update'),
 
 ]
